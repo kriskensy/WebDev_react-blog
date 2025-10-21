5237af11b005645345ef3b7b34ed4d411fa7ed57
kris.kensy <kriskensy@proton.me>
2025-10-21 19:59:12 +0200

    Add AddPostForm component

--- src/components/features/AddPostForm/AddPostForm.js
+++ src/components/features/AddPostForm/AddPostForm.js
@@ -2,12 +2,19 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { addPost } from "../../../redux/postsRedux";
 import PageTitle from "../../common/PageTitle/PageTitle";
-import { Container } from "react-bootstrap";
+import { Button, Container, Form } from "react-bootstrap";
+import { useState } from "react";
 
 const AddPostForm = () => {
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [published, setPublished] = useState('');
+  const [shortDescription, setShortDescription] = useState('');
+  const [mainContent, setMainContent] = useState('');
 
   const navigateToHome = () => {
     navigate('/');
@@ -16,14 +23,41 @@ const AddPostForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(addPost());
+    navigateToHome();
   }
-
+//TODO dokonczyc formularz, poprawic przesyłanie danych do reduxa
   return (
     <Container className="col-md-8 mx-auto">
       <PageTitle>AddPost</PageTitle>
-      <form onSubmit={handleSubmit}>
-      
-      </form>
+
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label>Title</Form.Label>
+          <Form.Control placeholder="Enter title" value={title} onChange={e => setTitle(e.target.value)}></Form.Control>
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Author</Form.Label>
+          <Form.Control placeholder="Enter author" value={author} onChange={e => setAuthor(e.target.value)}></Form.Control>
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Published</Form.Label>
+          <Form.Control placeholder="Enter date" value={published} onChange={e => setPublished(e.target.value)}></Form.Control>
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Short description</Form.Label>
+          <Form.Control as="textarea" rows={3} placeholder="Leave a comment here" value={shortDescription} onChange={e => setShortDescription(e.target.value)}></Form.Control>
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Main Content</Form.Label>
+          <Form.Control as="textarea" rows={8} placeholder="Leave a comment here" value={mainContent} onChange={e => setMainContent(e.target.value)}></Form.Control>
+        </Form.Group>
+        
+        <Button variant="primary" type="submit">Add post</Button>
+      </Form>
     </Container>
   );
 }
