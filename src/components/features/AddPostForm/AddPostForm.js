4e3cdccf60b4c182c9d85e56e659cfcfc26ad818
kris.kensy <kriskensy@proton.me>
2025-10-22 19:21:58 +0200

    Add PostForm conponent. Move Form and state from AddPostForm to PostForm

--- src/components/features/AddPostForm/AddPostForm.js
+++ src/components/features/AddPostForm/AddPostForm.js
@@ -2,68 +2,27 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { addPost } from "../../../redux/postsRedux";
 import PageTitle from "../../common/PageTitle/PageTitle";
-import { Button, Container, Form } from "react-bootstrap";
-import { useState } from "react";
+import { Container } from "react-bootstrap";
+import PostForm from "../PostForm/PostForm";
 
 const AddPostForm = () => {
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const [publishedDate, setPublishedDate] = useState('');
-  const [shortDescription, setShortDescription] = useState('');
-  const [content, setContent] = useState('');
-
   const navigateToHome = () => {
     navigate('/');
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch(addPost({
-      title,
-      shortDescription,
-      content,
-      publishedDate,
-      author
-    }));
+  const handleSubmit = (post) => {
+    dispatch(addPost(post));
     navigateToHome();
   }
-//TODO dokonczyc formularz, poprawic przesyłanie danych do reduxa
+
   return (
     <Container className="col-md-8 mx-auto">
       <PageTitle>Add a new post</PageTitle>
-
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3">
-          <Form.Label>Title</Form.Label>
-          <Form.Control placeholder="Enter title" value={title} onChange={e => setTitle(e.target.value)}></Form.Control>
-        </Form.Group>
-
-        <Form.Group className="mb-3">
-          <Form.Label>Author</Form.Label>
-          <Form.Control placeholder="Enter author" value={author} onChange={e => setAuthor(e.target.value)}></Form.Control>
-        </Form.Group>
-
-        <Form.Group className="mb-3">
-          <Form.Label>Published</Form.Label>
-          <Form.Control placeholder="Enter date" value={publishedDate} onChange={e => setPublishedDate(e.target.value)}></Form.Control>
-        </Form.Group>
-
-        <Form.Group className="mb-3">
-          <Form.Label>Short description</Form.Label>
-          <Form.Control as="textarea" rows={3} placeholder="Leave a comment here" value={shortDescription} onChange={e => setShortDescription(e.target.value)}></Form.Control>
-        </Form.Group>
-
-        <Form.Group className="mb-3">
-          <Form.Label>Main Content</Form.Label>
-          <Form.Control as="textarea" rows={8} placeholder="Leave a comment here" value={content} onChange={e => setContent(e.target.value)}></Form.Control>
-        </Form.Group>
-        
-        <Button variant="primary" type="submit">Add post</Button>
-      </Form>
+      <PostForm action={handleSubmit} actionText="Add post"/>
     </Container>
   );
 }
---
+++ src/components/features/PostForm/PostForm.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+
+const PostForm = ({ action, actionText, ...props }) => {
+
+  const [ title, setTitle] = useState(props.title || '');
+  const [ author, setAuthor ] = useState(props.author || '');
+  const [ publishedDate, setPublishedDate ] = useState(props.publishedDate || '');
+  const [ shortDescription, setShortDescription ] = useState(props.shortDescription || '');
+  const [ content, setContent ] = useState(props.content || '');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    action({ title, author, publishedDate, shortDescription, content });
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label>Title</Form.Label>
+          <Form.Control placeholder="Enter title" value={title} onChange={e => setTitle(e.target.value)}></Form.Control>
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Author</Form.Label>
+          <Form.Control placeholder="Enter author" value={author} onChange={e => setAuthor(e.target.value)}></Form.Control>
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Published</Form.Label>
+          <Form.Control placeholder="Enter date" value={publishedDate} onChange={e => setPublishedDate(e.target.value)}></Form.Control>
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Short description</Form.Label>
+          <Form.Control as="textarea" rows={3} placeholder="Leave a comment here" value={shortDescription} onChange={e => setShortDescription(e.target.value)}></Form.Control>
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Main Content</Form.Label>
+          <Form.Control as="textarea" rows={8} placeholder="Leave a comment here" value={content} onChange={e => setContent(e.target.value)}></Form.Control>
+        </Form.Group>
+        
+        <Button variant="primary" type="submit">Add post</Button>
+      </Form>
+  );
+}
+
+export default PostForm;
