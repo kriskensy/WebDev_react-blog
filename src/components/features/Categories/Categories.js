f1d32ea7e213e50718be38aeff2bd25dd7ef9e83
kris.kensy <kriskensy@proton.me>
2025-11-07 22:19:55 +0100

    Add category mapping to Categories component

--- src/components/features/Categories/Categories.js
+++ src/components/features/Categories/Categories.js
@@ -1,7 +1,30 @@
+import { ListGroup, Card } from "react-bootstrap";
+import PageTitle from "../../common/PageTitle/PageTitle";
+import { useSelector } from "react-redux";
+import { getAllCategories } from "../../../redux/categoriesRedux";
+import { Link } from "react-router-dom";
 
 const Categories = () => {
+
+  const categories = useSelector(getAllCategories);
   return (
-    <></>
+    <article className="col-md-8 mx-auto">
+      <PageTitle>All categories</PageTitle>
+      <Card>
+        <ListGroup variant="flush">
+          {categories.map(category => (
+            <ListGroup.Item
+              key={category.id}
+              category={category.categoryName}
+              as={Link}
+              to={`/category/${(category.categoryName).toLowerCase()}`}
+            >
+              {category.categoryName}
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </Card>
+    </article>
   );
 }
 
